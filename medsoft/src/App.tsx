e4e09b0ad0508ba9465ca74188a147f1b9437a1a
MeisZy <zyrusnw@gmail.com>
2024-03-19 14:25:37 +0800

    update: changed map size and navbar format

--- medsoft/src/App.tsx
+++ medsoft/src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Mapper from './components/Mapper'
-import Landing from './components/Landing'
 
 
 function App() {
 
   return (
     <>
-        <Landing/>
         <Navbar/>
         <Mapper/>
   </>
--- medsoft/src/components/Mapper.tsx
+++ medsoft/src/components/Mapper.tsx
@@ -1,65 +1,61 @@
 import './Mapper.css';
 import "leaflet/dist/leaflet.css";
+import Amogus from '../assets/images/mapmarker/amogus.png';
 
-import Amogus from '../assets/images/mapmarker/amogus.png'
-
-import { MapContainer,TileLayer, Marker, Popup} from 'react-leaflet';
-import {Icon} from "leaflet";
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import { Icon } from "leaflet";
 
 function Mapper(){
 
-  //named "markers" by default. DO NOT change the variable name.
   const markers = [
     {
-      geocode:[14.617290,121.059311],
+      geocode: [14.6258, 121.0617],
       popUp: "your very first react leaflet marker"
     },
     {
-      geocode:[14.618290,121.059311],
+      geocode: [14.6258, 121.0637],
       popUp: "your 2nd react leaflet marker"
     },
     {
-      geocode:[14.613290,121.059311],
+      geocode: [14.6258, 121.0657],
       popUp: "your 3rd react leaflet marker"
     },
   ]
 
   const customIcon = new Icon({
-    /*
-    iconUrl: require("path/of/image") works too.
-    */
-    iconUrl:(Amogus),
-    iconSize:[20,20]
+    iconUrl: Amogus,
+    iconSize: [20, 20]
   })
 
+  const origin = [14.6258, 121.0617]
 
   return(
     <>
       <MapContainer
-        center={[14.617290,121.059311]}
-        zoom={13}>
-        
-        <TileLayer
+      //@ts-expect-error ignore squiggly
+        center={origin}
+        zoom={13}
+      >
+        <TileLayer 
           url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         />
 
         {markers.map(marker => (         
           <Marker 
-          //@ts-expect-error ignore squiggly at attribute 'position'
+          //@ts-expect-error ignore position squggly
             position={marker.geocode}
             icon={customIcon}
           >
-          <Popup>{marker.popUp}</Popup>  
+            <Popup>{marker.popUp}</Popup>  
           </Marker>
         ))}
-
       </MapContainer>
     </>
   ) 
 }
-export default Mapper;
 
+export default Mapper;
   {/*
 
 const [mapHeight, setMapHeight] = useState(400); 
