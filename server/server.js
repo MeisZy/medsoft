22cb336fcadd7760f0b3c1b53621a29de311294b
MeisZy <zyrusnw@gmail.com>
2024-05-07 14:07:09 +0800

    restored

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-store');
+    next();
+  });
+
+const port = process.env.PORT || 4068;
+
+app.get("/", (req,res) => {
+    res.send({
+        onlineMessage: "You are currently online."
+    });
+})
+
+app.listen(port, () =>
+    console.log(`Medsoft express server now running on port ${port}`)
+    );
